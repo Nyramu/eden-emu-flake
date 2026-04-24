055a4f8b0046ddf095f334e426e326c2a52927c0
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-04-24 18:17:45 +0200

    refactor: merge homeModule.nix in default.nix to follow flake-parts convention

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,28 @@
 { self, ... }:
 {
-  flake.homeModules.default = import ../homeModule.nix self;
+  flake.homeModules.default =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      cfg = config.programs.eden;
+    in
+    {
+      options.programs.eden = {
+        enable = lib.mkEnableOption "Eden emulator";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.qt6Packages.callPackage (self + "/package.nix") { };
+          defaultText = lib.literalExpression "pkgs.qt6Packages.callPackage (self + \"/package.nix\") { }";
+          description = "The Eden package to use.";
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        home.packages = [ cfg.package ];
+      };
+    };
 }
